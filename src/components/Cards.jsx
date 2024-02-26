e1a37e309c71abd251b9b284cb92f27064a33dd7
CarlosDanielBenitez <cdanibenz@gmail.com>
2024-02-26 18:58:00 -0300

    Features: create helpers and question image

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import "../../src/App.css"
+import { getImages } from "../helpers/getImages";
+
+let size = 3;
+
 const Cards = () => {
 
-    const images = [
-        "/docker.svg",
-        "/bun.svg",
-        "/deno.svg"
-    ]
+    const [images, setImages] = useState(getImages());
+    const [selected, setSelected] = useState([]);
+    const [opened, setOpened] = useState([]);
 
-    const newImages = images.flatMap(item => [item, item]).
-        sort(() => Math.random())
+    const handleClick = (item) => {
+        setSelected(selected => selected.concat(item))
+
+    }
+
+    let include = false;
+
 
     return (
         <div className="cards">
             <h2>Score: 100</h2>
             <ul>
                 {
-                    newImages.map((item, index) => (
-                        <li key={index}>
+                    images.map((item, index) => (
+                        <li key={index} onClick={() => handleClick(item)}>
                             <div className="content">
+                                {include = selected.includes(item)}
                                 <div className="front">
+                                    <img src="/question.png" alt="" />
+                                </div>
+                                <div className="back">
                                     <img src={item} alt="" />
                                 </div>
                             </div>
---
+++ src/helpers/getImages.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const getImages = (size) => {
+
+    const images = [
+        "/docker.svg",
+        "/bun.svg",
+        "/deno.svg",
+        "/github.svg",
+        "/javascript.svg",
+        "/redis.svg",
+        "/supabase.svg",
+        "/svelte.svg",
+        "/vitejs.svg",
+        "/vscode.svg",
+    ]
+
+    const newImages = images.slice(0, size)
+
+    return newImages.flatMap(item => [item, item]).sort(() => Math.random() - 0.5)
+}
